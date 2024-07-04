56157bf9436f279aff4d275ef49c2c1e3f20813e
Gabriel Neves <gabrielneves.dev@gmail.com>
2024-07-04 12:10:47 -0300

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './containers/Home'
-// import App from './App.tsx'
-// import './index.css'
+// import Header from './containers/Header/'
+import GlobalStyles from './styles/globalStyles'
+import Home from './containers/Header/'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <GlobalStyles />
+    <Home></Home>
   </React.StrictMode>,
 )
