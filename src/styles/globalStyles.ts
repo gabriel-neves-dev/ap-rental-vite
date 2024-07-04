5808b67f49b16e2b67d8c7a26ccc7903e3765da5
Gabriel Neves <gabrielneves.dev@gmail.com>
2024-07-04 12:10:23 -0300

    global styles

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const globalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Inter';
+    }
+
+    
+
+
+
+`
+
+export default globalStyles;
